730a23fedb0e421b7ee5b5d71e9233977542667d
LandonChapin <l.chapin27@ncf.edu>
2024-12-08 17:45:34 -0500

    Added a few things

--- src/Control/GameController/GameController.java
+++ src/Control/GameController/GameController.java
@@ -6,7 +6,7 @@ import Display.StartScreen;
 public class GameController {
     private Player currentPlayer = Player.getPlayerInstance(); // Controls our Player
     private static GameController gameController = new GameController();
-        public static GameController getInstance(){return gameController;};
+        public static GameController getInstance(){return gameController;}
 
 
 
--- src/Display/StartScreen.java
+++ src/Display/StartScreen.java
@@ -2,6 +2,8 @@ package Display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class StartScreen {
     JFrame frame = new JFrame(); // Making a new screen
@@ -11,7 +13,8 @@ public class StartScreen {
     private static StartScreen startScreen = new StartScreen();
             public static StartScreen getStartScreen() {return startScreen;}
     private JLabel startText = new JLabel("You find yourself at the entrance to a large cavern");
-    private JTextField startButton = new JTextField();
+    private JTextField startButton = new JTextField(20);
+    private JButton inputButton = new JButton("Enter");
 
     public void initiateStartScreen(){
         frame.setSize(1300,1500);
@@ -20,17 +23,37 @@ public class StartScreen {
         frame.setIconImage(icon.getImage());
             System.out.println(icon); // Testing
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(0,0,0)); // Set the background color to blue
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(new Color(0, 0, 0)); // Set the background color to black
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10); // Add some padding between elements
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(startText, gbc);
+
+        gbc.gridy = 1;
+        panel.add(startButton, gbc);
+
+        gbc.gridy = 2;
+        panel.add(inputButton, gbc);
 
         frame.add(panel);
-
-
 
         // Text Stuff
         startText.setForeground(Color.WHITE);
 
-        //setting frame
-        frame.add(startText);
+        // Add action listener to the button
+        inputButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Uncomment the following code to enable the functionality
+
+                if (startButton.getText().equals("Enter The Cave")) {
+                    frame.setVisible(false);
+                }
+
+            }
+        });
     }
 }
