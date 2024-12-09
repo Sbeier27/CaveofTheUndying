759527d1ee55789e0559689ee35e34164cd1dedd
Sbeier27 <s.beier27@ncf.edu>
2024-12-09 13:51:32 -0500

    made the room builders but need help with them

--- src/Control/Rooms/PuzzleRoomBuilder.java
+++ src/Control/Rooms/PuzzleRoomBuilder.java
@@ -1,6 +1,6 @@
 package Control.Rooms;
 
-public class BasicRoomBuilder implements RoomBuilder{
+public class PuzzleRoomBuilder implements RoomBuilder{
 
     private Room room;
     private String interaction;
@@ -9,11 +9,8 @@ public class BasicRoomBuilder implements RoomBuilder{
     private RoomBuilder western;
     private RoomBuilder northern;
     private RoomBuilder southern;
-    private String puzzle;
-    private String answer;
-    private boolean solved = false;
 
-    public BasicRoomBuilder(){
+    public PuzzleRoomBuilder(){
         this.room = new Room();
     }
 
@@ -23,22 +20,15 @@ public class BasicRoomBuilder implements RoomBuilder{
         room.setCommand(interaction);
         if(interaction.equalsIgnoreCase("Interact")){
             return room.description;
-
         }else if(interaction.equalsIgnoreCase("Attack")){
             return "there is nothing to attack...you waste your strength";
         }else if(interaction.equalsIgnoreCase("Give up")) {
             return "you lay down to die, having lost faith in yourself";
-        } else if(interaction.equalsIgnoreCase("Examine")){
-            return puzzle;
-        } else if(interaction.equalsIgnoreCase(answer)){
-            solved = true;
-            return "you've solved the puzzle...somehow...continue";
-        }
         return null;
     }
 
     /* These commands are supposed to load the room to the proper direction if there is one
-    * and if there isn't a room in that direction display an appropriate message */
+     * and if there isn't a room in that direction display an appropriate message */
 
     @Override
     public void east(){
--- src/Control/Rooms/Room.java
+++ src/Control/Rooms/Room.java
@@ -5,6 +5,7 @@ public class Room {
     private String command;
     private String description;
     private Room nextRoom;
+    private Room prevRoom;
 
     public void setCommand(String command) {
         this.command = command;
@@ -16,5 +17,9 @@ public class Room {
 
     public void setNextRoom(Room nextRoom) {
         this.nextRoom = nextRoom;
+    }
+
+    public void setPrevRoom(Room prevRoom) {
+        this.prevRoom = prevRoom;
     }
 }
